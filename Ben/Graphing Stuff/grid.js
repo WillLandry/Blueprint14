22267a207b00620dba5b3fc00032f6c727169aac
carbidecannon <benschmitz1997@gmail.com>
2014-02-22 19:11:49 -0500

    grid update

--- Ben/Graphing Stuff/grid.js
+++ Ben/Graphing Stuff/grid.js
@@ -2,6 +2,6 @@ wwidth = window.innerWidth;
 wheight = window.innerHeight;
 var paper = Raphael(0, 0, wwidth, wheight);
 for(var x=0; x>=0; x=x+10)
- paper.rect(x, 0, 1, wheight);
+ paper.path("M"+x.toString()+", 0"+"V"+wheight.toString());
 for(var y=0; y>=0; y=y+10)
- paper.rect(0, y, wwidth, 1);
+ paper.path("M0,"+y.toString()+"H"+wwidth.toString());
